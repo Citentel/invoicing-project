8d4bbe03915d0f2c7d898f0bb6faff2d5d21b8e9
Citentel <gontrebor@gmail.com>
2020-10-24 05:18:11 +0200

    cosmetic changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './static/scss/app.css';
 
 export default function App() {
     return (
-        <div className={'app'}>
-            <div className={'container'}>
+        <div className="app">
+            <div className="container">
                 <h1 className="container__headline container__headline--1">
                     Hello App!
                 </h1>
