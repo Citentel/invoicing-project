e7f1a7ac5605f267045a403a5ddf4b387bb374b1
Citentel <gontrebor@gmail.com>
2020-10-24 04:05:49 +0200

    add basic style, create config scss for components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
-import '@csstools/normalize.css';
+import './static/normalize.css';
+import './static/scss/index.css';
 
 ReactDOM.render(
     <App />,
